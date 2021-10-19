20eb9fa235542eed32158667c746868800ff1dde
Christopher Angelo <angelo@angeloanan.xyz>
2021-10-20 06:24:17 +0700

    fix(prisma): fatal nextjs error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
+// https://github.com/prisma/prisma/issues/1983#issuecomment-620621213
 
-if (!prisma) {
-  prisma = new PrismaClient({
-    log: process.env.NODE_ENV === 'production' ? [] : ['error', 'info', 'query', 'warn'],
-  })
+let prisma
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      log: ['error', 'info', 'query', 'warn'],
+    })
+  }
+
+  prisma = global.prisma
 }
 
 export default prisma

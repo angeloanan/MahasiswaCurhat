2ca114f16c7d208221520b1555282e5265f00e74
Christopher Angelo <angelo@angeloanan.xyz>
2021-11-20 10:11:32 +0700

    feat(api): fetch all universities

---
+++ src/pages/api/universities.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { prisma } from '../../lib/prisma'
+
+async function Universities(_req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const universities = await prisma.university.findMany({
+      select: { name: true, students: false },
+    })
+
+    res.status(200).json(universities.map((university) => university.name))
+  } catch (error) {
+    res.status(500).json({ status: 500, error: error })
+  }
+}
+
+export default Universities

caf977540c62e0e643ae0ae3de8d610b81f1080f
Christopher Angelo <angelo@angeloanan.xyz>
2021-12-16 15:03:19 +0700

    feat: revert google callback uri

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -39,9 +39,7 @@ const SignIn = ({ providers }: SignInPageProps) => {
         <div className='flex gap-4 mt-8 w-full'>
           <button
             className='inline-flex justify-center py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white rounded-md border border-gray-300 shadow-sm hover:bg-gray-50'
-            onClick={() =>
-              signIn('google', { callbackUrl: 'https://curhat.space/api/auth/callback/google' })
-            }
+            onClick={() => signIn('google')}
           >
             <Google />
           </button>

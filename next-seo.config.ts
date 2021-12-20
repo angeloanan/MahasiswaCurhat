2bc853ee14344c8b303406477f1186273f61a3e7
Christopher Angelo <angelo@angeloanan.xyz>
2021-12-20 21:35:10 +0700

    feat: better seo

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,8 +1,8 @@
 import type { NextSeoProps } from 'next-seo'
 
 const SEO: NextSeoProps = {
-  defaultTitle: 'Mahasiswa Curhat',
-  titleTemplate: '%s | Mahasiswa Curhat',
+  defaultTitle: 'Mahasiswa Curhat - Curhat, Berbagi, Berdiskusi • curhat.space',
+  titleTemplate: '%s | Mahasiswa Curhat • curhat.space',
   description: 'A better way to cope with stress',
   openGraph: {
     title: 'Mahasiswa Curhat',

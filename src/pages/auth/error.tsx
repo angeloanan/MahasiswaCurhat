5832a51e423da2a98c20911166e61b1c40da7b7d
Christopher Angelo <angelo@angeloanan.xyz>
2021-12-29 22:22:26 +0700

    feat(auth-error): better error page

--- src/pages/auth/error.tsx
+++ src/pages/auth/error.tsx
@@ -1,12 +1,32 @@
 import * as React from 'react'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
-const AuthErrorPage: NextPage = (props) => {
+const nextAuthErrors = {
+  Signin: 'Try signing with a different account.',
+  OAuthSignin: 'Try signing with a different account.',
+  OAuthCallback: 'Try signing with a different account.',
+  OAuthCreateAccount: 'Try signing with a different account.',
+  EmailCreateAccount: 'Try signing with a different account.',
+  Callback: 'Try signing with a different account.',
+  OAuthAccountNotLinked:
+    'To confirm your identity, sign in with the same account you used originally.',
+  EmailSignin: 'Check your email address.',
+  CredentialsSignin: 'Sign in failed. Check the details you provided are correct.',
+  default: 'Unable to sign in.'
+}
+
+const AuthErrorPage: NextPage = () => {
+  const error = useRouter().query['error'] as keyof typeof nextAuthErrors
+  const errorMessage = error && (nextAuthErrors[error] ?? nextAuthErrors.default)
+
   return (
     <div className='mx-auto mt-8 w-full max-w-prose'>
-      <h1>Auth Error</h1>
-      <p>Something went wrong!</p>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+      <h1 className='text-4xl font-bold text-gray-800'>Authentication error</h1>
+      <p className='text-lg text-gray-600'>Something went wrong while signing you in:</p>
+      <pre className='mt-4'>
+        {error}: {JSON.stringify(errorMessage, null, 2)}
+      </pre>
     </div>
   )
 }
